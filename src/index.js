44d31d93b81b86e40c4b4143d8c19f27ce4a2d18
Samara <maiareismaia@gmail.com>
2022-04-26 21:29:48 -0300

    aula16 Webpack - Carregando arquivos CSS

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import $ from 'jquery';
+import './style.css';
 
 $(function(){
     $('#botao').on('click', function(){
         $('h1').html('Olá Mundo Alterado!')
+        $(this).addClass('botao');
     })
 });
